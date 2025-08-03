4a65a50713b3d95c142d6bb60169a7108407cd38
deboradevsouza <souza.deboravieira@gmail.com>
2025-08-03 12:35:41 -0300

    Adding home and pokemon pages

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Pikachu da Maromba</h2>
+        <h3>Pokédex</h3>
+        <h3>Gotta catch 'em all!</h3>
+        <p>Me ajude a zerar minha Pokédex me desafiando a uma série de exercícios com um Pokémon da lista.</p>
+
+        <NuxtLink to="/Pokemons/">
+            Ver Pokémons
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin: 20px;
+    font-size: 30px;
+}
+
+h3 {
+    margin: 20px;
+    font-size: 20px;
+}
+
+p {
+    margin: 20px;
+}
+</style>
